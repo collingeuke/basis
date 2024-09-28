ded1988bfa20954ee06f098badcb3a6d26d5d0a7
Collin Geuke <s1199236@student.windesheim.nl>
2024-09-28 14:07:17 +0200

    .

---
+++ opdrachten week 3/enc_temp_folder/14c010f82435192bac9d7f6605a6e5/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    int a = 0;
+    int b = 1;
+    int resultaat = 0;
+
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else {
+        for (int i = 2; i <= n; i++) {
+            resultaat = a + b;
+            a = b;
+            b = resultaat;
+        }
+        return resultaat;
+    }
+}
+
+int main() {
+    int index;
+
+    printf("Voer een index in: ");
+    scanf("%d", &index);
+
+    if (index < 0) {
+        printf("De index moet een niet-negatief getal zijn.\n");
+    }
+    else {
+        int resultaat = fibonacci(index);
+        printf("Het Fibonacci-getal op index %d is: %d\n", index, resultaat);
+    }
+
+    return 0;
+}
---
+++ opdrachten week 3/opdracht 8 week 3/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+
+
+main()
+{
+	int a = 1000;
+	double rente = 1.02;
+	float b, c, d, e, f, g, h, i, j, k, l, m, n, o, p;
+
+	b = a * rente;			
+	c = b * rente;		
+	d = c * rente;		
+	e = d * rente;		
+	f = e * rente;		
+	g = f * rente;		
+	h = g * rente;		
+	i = h * rente;		
+	j = i * rente;		
+	k = j * rente;		
+	l = k * rente;		
+	m = l * rente;		
+	n = m * rente;		
+	o = n * rente;	
+	p = o * rente;		
+
+
+	printf_s("jaar 1: %f\n", b);
+	printf_s("jaar 2: %f\n", c);	
+	printf_s("jaar 3: %f\n", d);
+	printf_s("jaar 4: %f\n", e);
+	printf_s("jaar 5: %f\n", f);
+	printf_s("jaar 6: %f\n", g);
+	printf_s("jaar 7: %f\n", h);
+	printf_s("jaar 8: %f\n", i);
+    printf_s("jaar 9: %f\n", j);
+	printf_s("jaar 10: %f\n", k);
+	printf_s("jaar 11: %f\n", l);
+	printf_s("jaar 12: %f\n", m);
+	printf_s("jaar 13: %f\n", n);
+	printf_s("jaar 14: %f\n", o);
+	printf_s("jaar 15: %f\n", p);
+	
+	
+
+}
+
