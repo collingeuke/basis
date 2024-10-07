023f47ac595c2dd040c2f0321cc104b2454be294
Collin Geuke <s1199236@student.windesheim.nl>
2024-10-07 14:26:33 +0200

    opdracht 3 week 4

--- opdrachten week 4/opdracht 3 week 4/main.c
+++ opdrachten week 4/opdracht 3 week 4/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 void Swap(int* a, int* b) {
-	int temp = a;
-	a = b;	
-	b = a;	
+	int temp = *a;
+	*a = *b;	
+	*b = temp;	
 
 	
 }
@@ -14,12 +14,10 @@ main()
 	int b = 10;
 	int resultaat[2];
 
-		Swap(a, b, resultaat);	
+		Swap(&a, &b);	
 
-		a = resultaat[0];
-		b = resultaat[1];
 
-		printf_s("%d, %d", a, b);
+		printf_s("a = %d, b = %d", a, b);
 
 	 
 
